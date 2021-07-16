cb0828d600ae98fe394cb90b376ccdfffb88d2a9
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-07-16 00:20:47 -0700

    navigation

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: '',
+    component: SigninComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
