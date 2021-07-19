f8163ba683c1377bdd2ad93844f7ff0d277b8454
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-07-19 00:37:22 -0700

    testing username funcaitonality

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -14,9 +14,24 @@ export class UniqueUsername implements AsyncValidator{
   validate = (control: FormControl) => {
     const { value } = control;
     // console.log(value);
-    console.log(this.http);
+    // console.log(this.http);
+    // return null;
 
-    return null;
+    //            generic annotation
+    // we are telling TS that we are getting any kind ov value
+    // as a result of this post request
+    return this.http.post<any>('https://api.angular-email.com/auth/username',
+      {
+        username: value
+      });
+    // open devtools and click on network request start typing a username
+    // after 3rd character, watch the validation happen sycnh then asynch
+    // in short
+    // angualr rules local validations then sends a request to the backend
+    // once the local validation had been passed
+
+    // if you enter "asdf" --- response will be  "in use"
+    // if you enter 'dude' --- response will be 'available 
   }
 }
 
